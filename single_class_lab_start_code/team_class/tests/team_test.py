b9c69daf8eef8099355da6c3001c20daedb4c37f
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-23 14:19:46 +0000

    third test passed

--- single_class_lab_start_code/team_class/tests/team_test.py
+++ single_class_lab_start_code/team_class/tests/team_test.py
@@ -16,7 +16,7 @@ class TestTeam(unittest.TestCase):
     def test_team_has_players(self):
         self.assertEqual(4, len(self.team.players))
 
-    @unittest.skip("delete this line to run the test")
+   # @unittest.skip("delete this line to run the test")
     def test_team_has_coach(self):
         self.assertEqual("Irv Blitzer", self.team.coach)
 
