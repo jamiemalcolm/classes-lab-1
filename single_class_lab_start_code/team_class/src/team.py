d9eeddf086482dc9fddd09942e70898c9e39189d
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-23 14:17:44 +0000

    has player method defined

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -7,3 +7,9 @@ class Team:
 
     def add_player(self, player_name):
         self.players.append(player_name)
+
+    def has_player(self, player_name):
+        for player in self.players:
+            if player == player_name:
+                return True
+        return False
