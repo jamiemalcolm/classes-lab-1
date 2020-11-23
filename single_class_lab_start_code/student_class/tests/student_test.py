631c4c4f58773974e3e37ed4e72d2380f1a893ed
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-23 14:13:09 +0000

    third test passed

--- single_class_lab_start_code/student_class/tests/student_test.py
+++ single_class_lab_start_code/student_class/tests/student_test.py
@@ -14,7 +14,7 @@ class TestStudent(unittest.TestCase):
     def test_student_has_cohort(self):
         self.assertEqual("E42", self.student.cohort)
 
-    @unittest.skip("delete this line to run the test")
+   # @unittest.skip("delete this line to run the test")
     def test_student_can_update_name(self):
         self.student.name = "Mike"
         self.assertEqual("Mike", self.student.name)
